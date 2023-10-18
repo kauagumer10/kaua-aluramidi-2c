e8480a091a265e64e6caf9ba9f813d03b233832d
kauagumer10 <119058470+kauagumer10@users.noreply.github.com>
2023-10-18 07:49:27 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -11,15 +11,17 @@ while (contador < listaDeTeclas.length) {
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
+    //template string
+    const idAudio = '#som_${instrumento}';
 
-    console.log(instrumento);
+    console.log(idAudio)
 
     tecla.onclick = function () {
-        tocaSom('#som_tecla_pom')
+        tocaSom(idAudio);
     }
 
     contador = contador + 1;
 
-    console.log(contador);
+    //console.log(contador);
 
 }
